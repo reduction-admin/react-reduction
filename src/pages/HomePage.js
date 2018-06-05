a081760c23d9a731fe217193e9715da1d972075d
deva <dvayyala@10.236.60.39>
2018-06-05 15:23:44 -0700

    added changes for nice overlay

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -18,6 +18,12 @@ import {
   ModalBody,
   ModalFooter,
   ModalHeader,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormText,
+  FormFeedback
 } from 'reactstrap';
 
 import { Line } from 'react-chartjs-2';
@@ -67,7 +73,7 @@ class CardPage extends React.Component {
   
   render() {
   return (
-    <Page title="Cards" breadcrumbs={[{ name: 'cards', active: true }]}>
+    <Page title="Cards" breadcrumbs={[{ name: 'cards', active: true }]} style={{marginTop: '80px'}}>
       <Row>
         <Col md={6} sm={6} xs={12} className="mb-3">
           <Card>
@@ -94,14 +100,63 @@ class CardPage extends React.Component {
                   North Carolina Tar Heels at Cal Bears Football <span style={{ color: '#00b5b5', fontSize: '16px'}}>(Sat, Sep 01, 2018)</span>
                 </ModalHeader>
                 <ModalBody>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                  sed do eiusmod tempor incididunt ut labore et dolore magna
-                  aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                  ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                  Duis aute irure dolor in reprehenderit in voluptate velit
-                  esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                  occaecat cupidatat non proident, sunt in culpa qui officia
-                  deserunt mollit anim id est laborum.
+                <Form className="col-sm-6">
+                  <FormGroup row>
+                    <Label for="exampleEmail" sm={4}>
+                      Bid per Ticket ($)
+                    </Label>
+                    <Col sm={8}>
+                      <Input
+                        type="email"
+                        name="email"
+                        placeholder="Enter Amount per Ticket"
+                      />
+                    </Col>
+                  </FormGroup>
+                  <FormGroup row>
+                    <Label for="exampleSelect" sm={4}>
+                      No.Of Tickets
+                    </Label>
+                    <Col sm={8}>
+                      <Input type="select" name="select">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>4</option>
+                        <option>6</option>
+                        <option>8</option>
+                        <option>8</option>
+                    </Input>
+                    </Col>
+                  </FormGroup>
+                  <FormGroup row>
+                    <Label for="exampleSelect" sm={4}>
+                      Bid Expires on
+                    </Label>
+                    <Col sm={8}>
+                      <Input type="select" name="select">
+                        <option>Event Date</option>
+                        <option>1 DAY</option>
+                        <option>2 DAYS</option>
+                        <option>4 DAYS</option>
+                        <option>6 DAYS</option>
+                        <option>10 DAYS</option>
+                    </Input>
+                    </Col>
+                  </FormGroup>
+                  <FormGroup row>
+                    <Label for="exampleText" sm={4}>
+                      Additional Notes
+                    </Label>
+                    <Col sm={8}>
+                      <Input type="textarea" name="text" />
+                    </Col>
+                  </FormGroup>
+                  <FormGroup check row>
+                    <Col sm={{ size: 10, offset: 9 }}>
+                      <Button>Place Bid</Button>
+                    </Col>
+                  </FormGroup>
+                </Form>
                 </ModalBody>
               </Modal>
             </CardBody>
