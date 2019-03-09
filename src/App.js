aafc3a3b842ef1fe626e9c1140da1b11fe30a5e1
simsim0709 <simsim0709@gmail.com>
2019-03-10 01:31:58 +0900

    Fix prettierrc settings

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import { STATE_LOGIN, STATE_SIGNUP } from "components/AuthForm";
-import GAListener from "components/GAListener";
-import { EmptyLayout, LayoutRoute, MainLayout } from "components/Layout";
-import AlertPage from "pages/AlertPage";
-import AuthModalPage from "pages/AuthModalPage";
-import AuthPage from "pages/AuthPage";
-import BadgePage from "pages/BadgePage";
-import ButtonGroupPage from "pages/ButtonGroupPage";
-import ButtonPage from "pages/ButtonPage";
-import CardPage from "pages/CardPage";
-import ChartPage from "pages/ChartPage";
+import { STATE_LOGIN, STATE_SIGNUP } from 'components/AuthForm';
+import GAListener from 'components/GAListener';
+import { EmptyLayout, LayoutRoute, MainLayout } from 'components/Layout';
+import AlertPage from 'pages/AlertPage';
+import AuthModalPage from 'pages/AuthModalPage';
+import AuthPage from 'pages/AuthPage';
+import BadgePage from 'pages/BadgePage';
+import ButtonGroupPage from 'pages/ButtonGroupPage';
+import ButtonPage from 'pages/ButtonPage';
+import CardPage from 'pages/CardPage';
+import ChartPage from 'pages/ChartPage';
 // pages
-import DashboardPage from "pages/DashboardPage";
-import DropdownPage from "pages/DropdownPage";
-import FormPage from "pages/FormPage";
-import InputGroupPage from "pages/InputGroupPage";
-import ModalPage from "pages/ModalPage";
-import ProgressPage from "pages/ProgressPage";
-import TablePage from "pages/TablePage";
-import TypographyPage from "pages/TypographyPage";
-import WidgetPage from "pages/WidgetPage";
-import React from "react";
-import componentQueries from "react-component-queries";
-import { BrowserRouter, Redirect, Switch } from "react-router-dom";
-import "./styles/reduction.scss";
+import DashboardPage from 'pages/DashboardPage';
+import DropdownPage from 'pages/DropdownPage';
+import FormPage from 'pages/FormPage';
+import InputGroupPage from 'pages/InputGroupPage';
+import ModalPage from 'pages/ModalPage';
+import ProgressPage from 'pages/ProgressPage';
+import TablePage from 'pages/TablePage';
+import TypographyPage from 'pages/TypographyPage';
+import WidgetPage from 'pages/WidgetPage';
+import React from 'react';
+import componentQueries from 'react-component-queries';
+import { BrowserRouter, Redirect, Switch } from 'react-router-dom';
+import './styles/reduction.scss';
 
 const getBasename = () => {
-  return `/${process.env.PUBLIC_URL.split("/").pop()}`;
+  return `/${process.env.PUBLIC_URL.split('/').pop()}`;
 };
 
 class App extends React.Component {
@@ -162,26 +162,26 @@ class App extends React.Component {
 
 const query = ({ width }) => {
   if (width < 575) {
-    return { breakpoint: "xs" };
+    return { breakpoint: 'xs' };
   }
 
   if (576 < width && width < 767) {
-    return { breakpoint: "sm" };
+    return { breakpoint: 'sm' };
   }
 
   if (768 < width && width < 991) {
-    return { breakpoint: "md" };
+    return { breakpoint: 'md' };
   }
 
   if (992 < width && width < 1199) {
-    return { breakpoint: "lg" };
+    return { breakpoint: 'lg' };
   }
 
   if (width > 1200) {
-    return { breakpoint: "xl" };
+    return { breakpoint: 'xl' };
   }
 
-  return { breakpoint: "xs" };
+  return { breakpoint: 'xs' };
 };
 
 export default componentQueries(query)(App);
