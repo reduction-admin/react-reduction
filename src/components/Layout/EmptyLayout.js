f26b929faf0072c5d188a6c783c91161d71e8293
Ilkwon Sim <simsim0709@gmail.com>
2019-03-10 01:28:47 +0900

    Update react scripts (#24)
    * Add .prettierrc
    
    * Update react-scripts to latest

--- src/components/Layout/EmptyLayout.js
+++ src/components/Layout/EmptyLayout.js
@@ -1,11 +1,9 @@
-import { Content } from 'components/Layout';
-import React from 'react';
+import { Content } from "components/Layout";
+import React from "react";
 
 const EmptyLayout = ({ children, ...restProps }) => (
   <main className="cr-app bg-light" {...restProps}>
-    <Content fluid onClick={this.handleContentClick}>
-      {children}
-    </Content>
+    <Content fluid>{children}</Content>
   </main>
 );
 
