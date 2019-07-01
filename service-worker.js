5288fa37fe699f0e04b88cd5107a976467a6dc1a
Ilkwon Sim (from Travis CI) <simsim0709@gmail.com>
2019-07-01 12:38:18 +0000

    Deploy reduction-admin/react-reduction to github.com/reduction-admin/react-reduction.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "https://reduction-admin.github.io/react-reduction/precache-manifest.b3403ca55b937c8b54461910a0fafe22.js"
+  "https://reduction-admin.github.io/react-reduction/precache-manifest.e6ea3d9e727d80607851565714cdd04e.js"
 );
 
 workbox.clientsClaim();
