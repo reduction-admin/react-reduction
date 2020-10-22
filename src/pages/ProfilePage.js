2089a25c5a3fe3af7a384905cb8ac5f61ed5b75b
khanadech007 <31152797+khanadech007@users.noreply.github.com>
2020-10-22 14:25:26 +0700

    Create ProfilePage.js

---
+++ src/pages/ProfilePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {
+  Row,
+  Col,
+  Button,
+  ButtonGroup,
+  Card,
+  CardHeader,
+  CardSubtitle,
+  CardBody,
+  CardText,
+} from 'reactstrap';
+
+import Page from 'components/Page';
